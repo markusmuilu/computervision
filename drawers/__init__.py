633373ea3bc9b7ccd27e3d09f8b54058c673ff87
Markus <markus.muilu@aalto.fi>
2026-09-13 22:07:53 +0300

    December 2025 computer vision work on NBA footage, unfinished

---
+++ drawers/__init__.py
@@ -0,0 +1,1 @@
+from .tracks_drawer import TracksDrawer
---
+++ utils/stubs_utils.py
@@ -0,0 +1,16 @@
+import os 
+import pickle
+
+def save_stub(stub_path, object):
+    if not os.path.exists(os.path.dirname(stub_path)):
+        os.makedirs(os.path.dirname(stub_path), exist_ok=True)
+    
+    if stub_path is not None:
+        with open(stub_path, 'wb') as f:
+            pickle.dump(object, f)
+
+def load_stub(read_from_stub, stub_path):
+    if read_from_stub is True and stub_path is not None and os.path.exists(stub_path):
+        with open(stub_path, 'rb') as f:
+            return pickle.load(f)
+    return None
